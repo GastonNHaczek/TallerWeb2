a7c0dea902a84ac47c87b94d5efaecf6888dfc0b
andrea-nd <descous.andrea@hotmail.com>
2022-10-29 13:46:21 -0300

    Login

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const conexion = require('../database');
 
+//login
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+const AWS = require('aws-sdk');
+const request = require('request');
+const jwkToPem = require('jwk-to-pem');
+const jwt = require('jsonwebtoken');
+global.fetch = require('node-fetch');
+
 //obtiene todos los productos
 router.get('/', function (req, res) {
     let sql = 'select * from Producto'
@@ -68,6 +77,40 @@ router.put('/:id', (req, res) => {
             res.json({status: 'producto modificado'});
         }
     })
+});
+
+//login
+const poolData = {    
+    UserPoolId : "us-east-2_NXGNelek7", // Your user pool id here    
+    ClientId : "3rcqik3ot5p7eeal5f7gdi38ba" // Your client id here
+    }; 
+const pool_region = 'us-east-2';
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+const app = express();
+
+app.use(express.json());
+
+router.post('/login', (req, res) => {
+    console.log("JSON:" + JSON.stringify(req.body));
+ 
+    var attributeList = [];
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email",Value:req.body.email}));
+
+    userPool.signUp(req.body.username, req.body.password, attributeList, null, function(err, result){
+        if (err) {
+            console.log(err);
+            res.json(err);
+            return;
+        }
+        cognitoUser = result.user;
+        console.log('user name is ' + cognitoUser.getUsername());
+        res.json({
+            bienvenido: `${cognitoUser.getUsername()}`
+        })
+    });
+   
 });
 
 module.exports = router
