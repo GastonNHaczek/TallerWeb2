2b6877069444dde966814b341a873a98dbf22624
lzelada <lucas.zeladaa@hotmail.com>
2022-11-12 11:24:49 -0300

    se modifico el alert del registro

--- src/app/registrar/registrar.component.ts
+++ src/app/registrar/registrar.component.ts
@@ -1,63 +1,44 @@
 import { Component, OnInit, Input, inject, Inject } from '@angular/core';
-import {Auth} from 'aws-amplify';
-import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService, Usuario } from '../servicio/LoginService';
-/*import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';*/
-
-
+import swal from 'sweetalert2';
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
   styleUrls: ['./registrar.component.css']
 })
-export class RegistrarComponent implements OnInit {
-  email:string;
-  password:string;
-  nombre:string;
-  apellido:string;
-  direccion:string;
-  constructor(private router:Router, private login:LoginService,
-    ) {
-    
-   }
 
-  ngOnInit(): void {
-  }
- /* async signUp(){
-    try {
-        const user  =  Auth.signUp({
-      username:this.email,
-      password:this.password,
-      attributes:{
+export class RegistrarComponent implements OnInit {
+
+  constructor(
+    private router:Router, 
+    private login:LoginService
+  ){}
+
+    email: string;
+    password: string;
+    nombre: string;
+    apellido: string;
+    direccion: string;
+    alerta: string = '';
+    ngOnInit(): void {
+    }
+
+    signUp(){
+      let usuario:Usuario;
+      usuario = {
+        username:this.email,
+        password:this.password,
         email:this.email,
         nombre:this.nombre,
         apellido:this.apellido,
         direccion:this.direccion
       }
-    });
-    console.log({user});
-      alert('User signup completed, please check verify your email');
+      
+      this.login.signUp(usuario).subscribe(); 
+
+      swal.fire('Se registro correctamenta, por favor verifica tu email' , this.alerta, 'success');
       this.router.navigate(['login']);
-  }catch(error){
-    console.log('error registro:', error);
   }
-  
-}*/
-signUp(){
-  let usuario:Usuario;
-  usuario={username:this.email,
-    password:this.password,
-    email:this.email,
-    nombre:this.nombre,
-    apellido:this.apellido,
-    direccion:this.direccion}
-  this.login.signUp(usuario).subscribe(); 
-
-  console.log({usuario});
-      alert('User signup completed, please check verify your email');
-  this.router.navigate(['login']);
-
-}
 
 }
