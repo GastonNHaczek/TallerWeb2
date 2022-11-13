f9d150b8e31e0bd63c46f6a8cdfd884a44659c2d
andrea-nd <descous.andrea@hotmail.com>
2022-11-13 20:42:46 -0300

    Logo carrito

--- backend/database.js
+++ backend/database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'bariloche10',
-    database: 'angular',
+    password: 'root',
+    database: 'andreaDB',
     port: 3306
 });
 connection.connect(function(error){
